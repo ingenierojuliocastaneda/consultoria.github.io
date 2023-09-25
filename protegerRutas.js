8d4b0215702f4060c2833edd4f3b2d085545f9fb
ingenierojuliocastaneda <57018466+ingenierojuliocastaneda@users.noreply.github.com>
2023-09-25 13:44:37 -0500

    Commit JS

---
+++ protegerRutas.js
@@ -0,0 +1,29 @@
+// Define las rutas protegidas
+const rutasProtegidas = ['/index.html'];
+// Verifica si el usuario está autenticado (puedes usar sessionStorage o localStorage)
+const usuarioAutenticado = sessionStorage.getItem('usuarioAutenticado');
+// Función para verificar el acceso a rutas protegidas
+function verificarAcceso() {
+    // Obtén la ruta actual
+    const rutaActual = window.location.pathname;
+  
+    // Verifica si la ruta actual está en la lista de rutas protegidas y si el usuario no está autenticado
+    if (rutasProtegidas.includes(rutaActual) && !usuarioAutenticado) {
+      // Redirige al usuario a la página de inicio de sesión (cambia "inicio-de-sesion.html" por tu URL real)
+      window.location.href = '/Logged_out.html';
+    }
+  }
+  
+  // Llama a la función para verificar el acceso cuando la página se carga
+  verificarAcceso();
+
+  // Cuando el usuario inicia sesión con éxito
+sessionStorage.setItem('usuarioAutenticado', 'true');
+
+// Cuando el usuario cierra sesión
+sessionStorage.removeItem('usuarioAutenticado');
+
+// Redirige al usuario a la página de inicio de sesión
+window.location.href = '/Logged_out.html';
+
+  
